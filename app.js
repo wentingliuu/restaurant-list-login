377cc53ff5636ead67abd089d3d7b2918a43a3ef
wentingliu <cindy810317@gmail.com>
2021-09-23 16:52:26 +0800

    feat: add update function

--- app.js
+++ app.js
@@ -57,6 +57,41 @@ app.get('/restaurants/:id', (req, res) => {
            .then(restaurant => res.render('detail', {restaurant}))
            .catch(error => console.log(error))
 })
+// edit.hbs (update)
+app.get('/restaurants/:id/edit', (req,res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+           .lean()
+           .then(restaurant => res.render('edit', {restaurant}))
+           .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.findById(id)
+           .then(restaurant => {
+             restaurant.name = name
+             restaurant.name_en = name_en
+             restaurant.category = category
+             restaurant.image = image
+             restaurant.location = location
+             restaurant.phone = phone
+             restaurant.google_map = google_map
+             restaurant.rating = rating
+             restaurant.description = description
+             return restaurant.save()
+           })
+           .then(() => res.redirect(`/restaurants/${id}`))
+           .catch(error => console.log(error))
+})
 
 // start and listen on the Express server ///
 app.listen(port, () => {
