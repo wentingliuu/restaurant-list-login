a386c1d27f7a5e938361c31c3382decb364724cd
wentingliu <cindy810317@gmail.com>
2021-09-26 18:19:05 +0800

    fix: follow javaScript standard style

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,18 +1,18 @@
-//Require Mongoose
-var mongoose = require('mongoose')
+// Require Mongoose
+const mongoose = require('mongoose')
 
 // Define schema
 const Schema = mongoose.Schema
-const restaurantSchema = new Schema ({
-  name: {type: String, required: true},
-  name_en : {type: String, required: true},
-  category: {type: String, required: true},
-  image: {type: String, required: true},
-  location: {type: String, required: true},
-  phone: {type: String, required: true},
-  google_map: {type: String, required: true},
-  rating: {type: Number, required: true},
-  description: {type: String, required: true}
+const restaurantSchema = new Schema({
+  name: { type: String, required: true },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
+  google_map: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true }
 })
 
 // Compile model from schema
--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -6,11 +6,11 @@ const restaurantData = restaurantList.results
 mongoose.connect('mongodb://localhost/restaurant-list')
 const db = mongoose.connection
 
-db.on('error', () => {console.log('MongoDB error!')})
+db.on('error', () => { console.log('MongoDB error!') })
 db.once('open', () => {
   console.log('MongoDB connected!')
   for (let i = 0; i < restaurantData.length; i++) {
-    Restaurant.create({  
+    Restaurant.create({
       name: restaurantData[i].name,
       name_en: restaurantData[i].name_en,
       category: restaurantData[i].category,
@@ -19,8 +19,8 @@ db.once('open', () => {
       phone: restaurantData[i].phone,
       google_map: restaurantData[i].google_map,
       rating: restaurantData[i].rating,
-      description: restaurantData[i].description})
+      description: restaurantData[i].description
+    })
   }
   console.log('Date created!')
 })
-
