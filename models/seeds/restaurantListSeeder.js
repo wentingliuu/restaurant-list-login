016b59a25cbd73a6ce118ee757e7384beda30a9d
wentingliu <cindy810317@gmail.com>
2021-10-24 00:20:55 +0800

    modify restaurantListSeeder

--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -1,21 +1,53 @@
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
 
+const User = require('../userSchema')
 const Restaurant = require('../restaurantSchema')
 const restaurantList = require('./restaurant.json').results
 
-db.once('open', () => {
-  for (let i = 0; i < restaurantList.length; i++) {
-    Restaurant.create({
-      name: restaurantList[i].name,
-      name_en: restaurantList[i].name_en,
-      category: restaurantList[i].category,
-      image: restaurantList[i].image,
-      location: restaurantList[i].location,
-      phone: restaurantList[i].phone,
-      google_map: restaurantList[i].google_map,
-      rating: restaurantList[i].rating,
-      description: restaurantList[i].description
-    })
+const seeders = [
+  {
+    email: 'user1@example.com',
+    password: '12345678',
+    index: [1, 2, 3]
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678',
+    index: [4, 5, 6]
+  },
+  {
+    name: '老饕',
+    email: 'root@example.com',
+    password: '12345678',
+    index: [1, 2, 3, 4, 5, 6, 7, 8, 9]
   }
-  console.log('Data created!')
+]
+
+db.once('open', () => {
+  Promise.all(
+    Array.from(seeders, seeder => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(seeder.password, salt))
+        .then(hash => User.create({
+          name: seeder.name || '吃貨',
+          email: seeder.email,
+          password: hash
+        }))
+        .then(user => {
+          const userRestaurantList = []
+          Promise.all(Array.from(seeder.index, i => {
+            const restaurantData = restaurantList.find(restaurant => restaurant.id === i)
+            restaurantData.userId = user._id
+            userRestaurantList.push(restaurantData)
+          }))
+          return Restaurant.create(userRestaurantList)
+        })
+    }))
+    .then(() => {
+      console.log('Data created.')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
